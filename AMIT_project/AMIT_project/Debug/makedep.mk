54e5d50bf830346160abd71eed4536e17bb0e1bd
mazoon2xx1 <119125408+mazoon2xx1@users.noreply.github.com>
2023-06-29 19:44:03 +0300

    Add files via upload

---
+++ AMIT_project/AMIT_project/Debug/makedep.mk
@@ -0,0 +1,16 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+HHAL\H7segment\H7segment-program.c
+
+HHAL\PB\PB-program.c
+
+main.c
+
+MCAL\MADC\MADC_Program.c
+
+MCAL\MEEPROM\MEEPROM-program.c
+
+MCAL\MYDELAY\MYDELAY-program.c
+
